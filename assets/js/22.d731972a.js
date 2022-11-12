2888db28ce6797e31ef81b2e8735c87c9b6f9c49
mangopay2022 <116899333+mangopay2022@users.noreply.github.com>
2022-11-12 19:14:19 +0800

    Update 22.d731972a.js

--- assets/js/22.d731972a.js
+++ assets/js/22.d731972a.js
@@ -1,1 +1,27 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[22],{288:function(t,n,v){"use strict";v.r(n);var _=v(13),h=Object(_.a)({},(function(){var t=this,n=t._self._c;return n("ContentSlotsDistributor",{attrs:{"slot-key":t.$parent.slotKey}},[n("h1",{attrs:{id:"银行列表-越南-vietnam"}},[n("a",{staticClass:"header-anchor",attrs:{href:"#银行列表-越南-vietnam"}},[t._v("#")]),t._v(" 银行列表 (越南 Vietnam)")]),t._v(" "),n("table",[n("thead",[n("tr",[n("th",[t._v("编号")]),t._v(" "),n("th",[t._v("名称")])])]),t._v(" "),n("tbody",[n("tr",[n("td",[t._v("356")]),t._v(" "),n("td",[t._v("Ngân hàng TMCP Á Châu")])]),t._v(" "),n("tr",[n("td",[t._v("357")]),t._v(" "),n("td",[t._v("Ngân hàng Thương mại Cổ phần Tiên Phong")])]),t._v(" "),n("tr",[n("td",[t._v("358")]),t._v(" "),n("td",[t._v("Ngân hàng Thương mại Cổ phần Kỹ thương Việt Nam")])]),t._v(" "),n("tr",[n("td",[t._v("359")]),t._v(" "),n("td",[t._v("Ngân hàng TMCP Nam Á")])]),t._v(" "),n("tr",[n("td",[t._v("360")]),t._v(" "),n("td",[t._v("NGÂN HÀNG THƯƠNG MẠI CỔ PHẦN VIỆT NAM THỊNH VƯỢNG")])]),t._v(" "),n("tr",[n("td",[t._v("361")]),t._v(" "),n("td",[t._v("Ngân hàng thương mại cổ phần Phát triển Thành phố Hồ Chí Minh")])]),t._v(" "),n("tr",[n("td",[t._v("362")]),t._v(" "),n("td",[t._v("Ngân hàng Thương mại Cổ phần Quốc Tế Việt Nam")])]),t._v(" "),n("tr",[n("td",[t._v("363")]),t._v(" "),n("td",[t._v("Ngân hàng TMCP Sài Gòn-Hà Nội")])]),t._v(" "),n("tr",[n("td",[t._v("364")]),t._v(" "),n("td",[t._v("Ngân hàng thương mại cổ phần Sài Gòn Thương Tín")])]),t._v(" "),n("tr",[n("td",[t._v("365")]),t._v(" "),n("td",[t._v("Ngân hàng Công thương Việt Nam")])]),t._v(" "),n("tr",[n("td",[t._v("366")]),t._v(" "),n("td",[t._v("Ngân hàng TMCP Ngoại thương Việt Nam")])]),t._v(" "),n("tr",[n("td",[t._v("367")]),t._v(" "),n("td",[t._v("NGÂN HÀNG THƯƠNG MẠI CỔ PHẦN CÔNG THƯƠNG VIỆT NAM")])]),t._v(" "),n("tr",[n("td",[t._v("368")]),t._v(" "),n("td",[t._v("Ngân hàng TMCP Đầu tư và Phát triển Việt Nam")])]),t._v(" "),n("tr",[n("td",[t._v("369")]),t._v(" "),n("td",[t._v("Ngân hàng QUÂN ĐỘI")])]),t._v(" "),n("tr",[n("td",[t._v("370")]),t._v(" "),n("td",[t._v("Ngân hàng Nông nghiệp và Phát triển Nông thôn Việt Nam")])])])])])}),[],!1,null,null,null);n.default=h.exports}}]);
+(window.webpackJsonp = window.webpackJsonp || []).push([[22], {
+    288: function(t, n, v) {
+        "use strict";
+        v.r(n);
+        var _ = v(13)
+          , h = Object(_.a)({}, (function() {
+            var t = this
+              , n = t._self._c;
+            return n("ContentSlotsDistributor", {
+                attrs: {
+                    "slot-key": t.$parent.slotKey
+                }
+            }, [n("h1", {
+                attrs: {
+                    id: "银行列表-越南-vietnam"
+                }
+            }, [n("a", {
+                staticClass: "header-anchor",
+                attrs: {
+                    href: "#银行列表-越南-vietnam"
+                }
+            }, [t._v("#")]), t._v(" 银行列表 (越南 Vietnam)")]), t._v(" "), n("table", [n("thead", [n("tr", [n("th", [t._v("编号")]), t._v(" "), n("th", [t._v("名称")])])]), t._v(" "), n("tbody", [n("tr", [n("td", [t._v("468")]), t._v(" "), n("td", [t._v("NGÂN HÀNG ACB")])]), t._v(" "), n("tr", [n("td", [t._v("469")]), t._v(" "), n("td", [t._v("NGÂN HÀNG TPBank")])]), t._v(" "), n("tr", [n("td", [t._v("470")]), t._v(" "), n("td", [t._v("NGÂN HÀNG TechcomBank")])]), t._v(" "), n("tr", [n("td", [t._v("471")]), t._v(" "), n("td", [t._v("NGÂN HÀNG Nam A Bank")])]), t._v(" "), n("tr", [n("td", [t._v("472")]), t._v(" "), n("td", [t._v("NGÂN HÀNG VPBank")])]), t._v(" "), n("tr", [n("td", [t._v("473")]), t._v(" "), n("td", [t._v("NGÂN HÀNG HDBank")])]), t._v(" "), n("tr", [n("td", [t._v("474")]), t._v(" "), n("td", [t._v("NGÂN HÀNG VIB")])]), t._v(" "), n("tr", [n("td", [t._v("475")]), t._v(" "), n("td", [t._v("NGÂN HÀNG SHB")])]), t._v(" "), n("tr", [n("td", [t._v("476")]), t._v(" "), n("td", [t._v("NGÂN HÀNG SacomBank")])]), t._v(" "), n("tr", [n("td", [t._v("477")]), t._v(" "), n("td", [t._v("Ngân hàng Công thương Việt Nam")])]), t._v(" "), n("tr", [n("td", [t._v("478")]), t._v(" "), n("td", [t._v("NGÂN HÀNG VietcomBank")])]), t._v(" "), n("tr", [n("td", [t._v("479")]), t._v(" "), n("td", [t._v("NGÂN HÀNG VietinBank")])]), t._v(" "), n("tr", [n("td", [t._v("480")]), t._v(" "), n("td", [t._v("NGÂN HÀNG BIDV")])]), t._v(" "), n("tr", [n("td", [t._v("481")]), t._v(" "), n("td", [t._v("NGÂN HÀNG Mb Bank")])]), t._v(" "), n("tr", [n("td", [t._v("482")]), t._v(" "), n("td", [t._v("NGÂN HÀNG AgriBank")])])])])])
+        }
+        ), [], !1, null, null, null);
+        n.default = h.exports
+    }
+}]);
